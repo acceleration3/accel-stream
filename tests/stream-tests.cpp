bd79e21d3252a86a043da034b5156f83dcf24c27
acceleration3 <acceleration23@gmail.com>
2023-10-07 13:32:58 +0100

    Rework memory streams to be able to be constructed from any kind of memory. Previous memory stream is now called dynamic stream.  Byte type. 64-bit file seek and tell support. UTF-8 path support.

--- tests/stream-tests.cpp
+++ tests/stream-tests.cpp
@@ -8,7 +8,6 @@ using namespace accel;
 
 int main(int argc, char* argv[])
 {
-	// File output stream
 	{
 		stream::file_output_stream stream("test.bin", stream::file_modes::binary);
 		for (std::size_t i = 0; i < 30; i++)
@@ -17,7 +16,6 @@ int main(int argc, char* argv[])
 		stream << "This is a test string.";
 	}
 
-	// File input stream
 	{
 		stream::file_input_stream stream("test.bin", stream::file_modes::binary);
 
@@ -33,10 +31,8 @@ int main(int argc, char* argv[])
 		assert(test == "This is a test string.");
 	}
 
-
-	// Memory input stream
 	{
-		std::vector<char> test = { 0x01, 0x00, 0x03, 0x00 };
+		char test[4] = { 0x01, 0x00, 0x03, 0x00 };
 		stream::memory_input_stream stream(test);
 
 		std::uint16_t a = 0;
@@ -48,14 +44,15 @@ int main(int argc, char* argv[])
 	}
 
 	{
-		stream::memory_output_stream stream(10);
+		char test[6];
+		stream::memory_output_stream stream(test);
 
 		std::uint16_t a = 0x0001;
 		std::uint16_t b = 0x0002;
 		stream.write_object_be(a);
 		stream.write_object_be(b);
 
-		auto data = stream.data();
+		auto data = stream.get_data();
 		assert(data[0] == 0);
 		assert(data[1] == 1);
 		assert(data[2] == 0);
@@ -64,13 +61,40 @@ int main(int argc, char* argv[])
 		stream.seek_write(2, stream::seek_references::begining);
 		stream.write_object_le(4);
 
-		data = stream.data();
+		data = stream.get_data();
 		assert(data[0] == 0);
 		assert(data[1] == 1);
 		assert(data[2] == 4);
 		assert(data[3] == 0);
 		assert(data[4] == 0);
 		assert(data[5] == 0);
+	}
+
+	{
+		std::uint16_t a = 0x0001;
+		std::uint16_t b = 0x0002;
+		stream::dynamic_output_stream stream;
+		stream.write_object_be(a);
+		stream.write_object_be(b);
+		auto data = stream.get_data();
+		assert(stream.get_size() == 4);
+		assert(data[0] == 0);
+		assert(data[1] == 1);
+		assert(data[2] == 0);
+		assert(data[3] == 2);
+		stream.write_object_le(b);
+		stream.write_object_le(a);
+
+		data = stream.get_data();
+		assert(stream.get_size() == 8);
+		assert(data[0] == 0);
+		assert(data[1] == 1);
+		assert(data[2] == 0);
+		assert(data[3] == 2);
+		assert(data[4] == 2);
+		assert(data[5] == 0);
+		assert(data[6] == 1);
+		assert(data[7] == 0);
 	}
 
 	std::cout << "All tests completed sucessfully.";
